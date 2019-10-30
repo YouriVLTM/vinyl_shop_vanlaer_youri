794a9352ddc635fb6ffd068697d755968e526718
YouriVLTM <r0694461@student.thomasmore.be>
2019-10-30 19:23:32 +0100

    Opdracht 1: <= Basic controllers

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<h1>Contact info</h1>
+
+<p>The Vinyl Shop</p>
+<p><a href="mailto:info@yourivanlaer.be">info@yourivanlaer.be</a></p>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1>The Vinyl Shop</h1>
+
+<p>Welcome to the website of... </p>
